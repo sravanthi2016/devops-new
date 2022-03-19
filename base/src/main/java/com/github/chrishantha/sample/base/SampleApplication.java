ad042b7b8a117b599508760deef8a3e4cd7fbd77
Sagar1047 <kundarapusagar@gmail.com>
2022-03-19 14:00:54 +0530

    UNKONEN COMMIT

--- base/src/main/java/com/github/chrishantha/sample/base/SampleApplication.java
+++ base/src/main/java/com/github/chrishantha/sample/base/SampleApplication.java
@@ -12,11 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */s ]
 package com.github.chrishantha.sample.base;
 
-public interface SampleApplication {
-
+public interface SampleApplication {fff\\
+kmkkjhngdyfgryagfGHUEQIU
     void start();
 
     String toString();
